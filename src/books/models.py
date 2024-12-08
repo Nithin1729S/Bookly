8ff6e8bf22b5cd4a47f43f793b8b84b00d9a7bde
Nithin1729S <sureshnithin1729@gmail.com>
2024-12-08 17:26:21 +0530

    dependency injection

--- src/books/models.py
+++ src/books/models.py
@@ -1,14 +1,27 @@
-from sqlmodel import SQLModel
-from datetime import datetime
+from sqlmodel import SQLModel,Field,Column
+import sqlalchemy.dialects.postgresql as pg
+from datetime import date, datetime
 import uuid
+
 class Book(SQLModel,table=True):
-    uid: uuid.UUID
+    __tablename__="books"
+    
+    uid: uuid.UUID=Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4()
+        )
+    )
     title: str
     author  : str
     publisher: str
-    published_date: str
+    published_date: date
     page_count: int
     language:str
-    created_at:datetime
-    updated_at:datetime
+    created_at:datetime=Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now()))
+    updated_at:datetime=Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now()))
 
+    def __repr__(self):
+        return f"{self.title} by {self.author}"
